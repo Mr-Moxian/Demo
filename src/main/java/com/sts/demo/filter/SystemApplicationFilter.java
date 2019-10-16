5dfd21a47c16ced7232091902508d09aabd6ddb6
Mr-Moxian <1090899007@qq.com>
2019-10-16 11:32:39 +0800

    登录界面优化提交

--- src/main/java/com/sts/demo/filter/SystemApplicationFilter.java
+++ src/main/java/com/sts/demo/filter/SystemApplicationFilter.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 @WebFilter(filterName = "SystemApplicationFilter", urlPatterns = {"/*"},
@@ -20,8 +22,12 @@ import java.util.List;
                         value = "/user/login#/user/registerUser")})
 public class SystemApplicationFilter implements Filter {
 
-    private static final String FILTER_PATH = "filterPath";
-    private static final String IGNORED_PATH = "ignoredUrl";
+    //发现通过注解注入的参数没法读取，暂先注掉，有时间排查
+    //private static final String FILTER_PATH = "filterPath";
+    //private static final String IGNORED_PATH = "ignoredUrl";
+
+    private static final String FILTER_PATH = "/user/login#/user/registerUser";
+    private static final String IGNORED_PATH = ".css#.js#.jpg#.png#.gif#.ico#.woff2#.eot";
 
     private static final List<String> ignoredList = new ArrayList<>();
     private String[] allowUrls;
@@ -30,7 +36,7 @@ public class SystemApplicationFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         //初始化被允许的url以及不进行过滤的url类型
-        String filterPath = filterConfig.getInitParameter(FILTER_PATH);
+        /*String filterPath = filterConfig.getInitParameter(FILTER_PATH);
         if (!StringUtils.isEmpty(filterPath)) {
             allowUrls = filterPath.contains("#") ? filterPath.split("#") : new String[]{filterPath};
         }
@@ -38,10 +44,15 @@ public class SystemApplicationFilter implements Filter {
         String ignoredPath = filterConfig.getInitParameter(IGNORED_PATH);
         if (!StringUtils.isEmpty(ignoredPath)) {
             ignoredUrls = ignoredPath.contains("#") ? ignoredPath.split("#") : new String[]{ignoredPath};
-//            for (String ignoredUrl : ignoredUrls) {
-//                ignoredList.add(ignoredUrl);
-//            }
+            ignoredList.addAll(Arrays.asList(ignoredUrls));
+        }*/
 
+        if (!StringUtils.isEmpty(FILTER_PATH)) {
+            allowUrls = FILTER_PATH.contains("#") ? FILTER_PATH.split("#") : new String[]{FILTER_PATH};
+        }
+
+        if (!StringUtils.isEmpty(IGNORED_PATH)) {
+            ignoredUrls = IGNORED_PATH.contains("#") ? IGNORED_PATH.split("#") : new String[]{IGNORED_PATH};
             ignoredList.addAll(Arrays.asList(ignoredUrls));
         }
     }
@@ -58,6 +69,11 @@ public class SystemApplicationFilter implements Filter {
             chain.doFilter(request, response);
             return;
         }
+
+        if(verify(ignoredList,requestUrl)){
+            chain.doFilter(request, response);
+            return;
+        }
         if(requestUrl != null && requestUrl.equals("/login.html")){
             chain.doFilter(request, response);
             return;
@@ -68,5 +84,19 @@ public class SystemApplicationFilter implements Filter {
     @Override
     public void destroy() {
 
+    }
+
+    private static String regexPrefix = "^.*";
+    private static String regexSuffix = ".*$";
+
+    private static boolean verify(List<String> ignoredList, String url) {
+        for (String regex : ignoredList) {
+            Pattern pattern = Pattern.compile(regexPrefix + regex + regexSuffix);
+            Matcher matcher = pattern.matcher(url);
+            if (matcher.matches()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
